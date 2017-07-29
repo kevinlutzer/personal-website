873c83fba4f2ecf3a6cf5f4a56b8a2eb58cd5b9b
Kevin Lutzer <kml183@mail.usask.ca>
2017-07-28 19:20:52 -0600

    set the services as providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {ProjectService, ProjectApiStubService, Project} from './project';
-import {VisitorService, VisitorApiStubService, Visitor} from './visitor';
+import {ProjectService, ProjectApiService, ProjectApiStubService, Project} from './project';
+import {VisitorService, VisitorApiService, VisitorApiStubService, Visitor} from './visitor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    VisitorService,
+    ProjectService,
+    {provide: VisitorApiService, useClass: VisitorApiStubService},
+    {provide: ProjectApiService, useClass: ProjectApiStubService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
