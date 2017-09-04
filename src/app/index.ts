deafc8fcb7a97e135157852cc86aacf9263b9e6d
Kevin Lutzer <kml183@mail.usask.ca>
2017-09-03 19:38:41 -0600

    move index

--- src/app/index.ts
+++
@@ -1,6 +0,0 @@
-export { Activity, ActivityApiService, ActivityService, ActivityStubService } from './activity';
-export { Project, ProjectApiService, ProjectService, ProjectApiStubService } from './project';
-export { Visitor, VisitorType, VisitorOptions, VisitorApiService, VisitorService, VisitorApiStubService } from './visitor';
-export { AlertService, AlertModule } from './alert';
-
-export { Item } from './item';
