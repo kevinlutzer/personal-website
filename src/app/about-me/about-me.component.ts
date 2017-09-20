bfed119d160b21489cf0909863a7315260469aa0
Kevin Lutzer <kml183@mail.usask.ca>
2017-09-20 06:27:39 -0600

    get the experience stream on the about me page.

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable'
+
+import { ActivityService, Activity } from '../activity';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about-me.component.html'
 })
 export class AboutMeComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() {}
-  
+  items: Observable<Item[]>;
+
+  constructor(
+    private activityService: ActivityService
+  ) { }
+
+  ngOnInit() {
+    this.items = this.activityService
+      .list()
+      .map(activities => this.getDisplayItems(activities));
+
+    this.items.subscribe(item => console.log(item));
+  }
+
+  private getDisplayItems(activities: Activity[]): Item[] {
+    return activities.map(activity => {
+        return {
+          title: activity.name,
+          subTitle: activity.location,
+          description: activity.description,
+          content: activity.content,
+          startDate: activity.dateStart,
+          endDate: activity.dateFinish,
+          imageUrl: activity.activityImgUrl
+        };
+    });
+}
 }
