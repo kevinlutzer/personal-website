56ad954f00e1088d5465a17cc56994ba068b918c
Kevin Lutzer <kml183@mail.usask.ca>
2017-09-19 21:07:31 -0600

    last commit

--- src/app/project/projects/projects.component.ts
+++ src/app/project/projects/projects.component.ts
@@ -4,22 +4,40 @@ import { Observable } from 'rxjs/Observable';
 import { Project } from '../shared/project.model';
 import { ProjectService } from '../shared/project.service';
 
+import { Item } from '../../item';
+
 @Component({
     selector: 'projects',
     template: `
-        hello world
+        <item-list [items]="projectItems | async"></item-list>
     `,
 })
 
 export class ProjectsComponent implements OnInit {
 
     private projects: Observable<Project[]>;
+    private projectItems: Observable<Item[]>;
 
     constructor(
         private projectsService: ProjectService
     ) {}
 
+    private getDisplayItems(projects: Project[]): Item[] {
+        return projects.map(project => {
+            return {
+                title: project.name,
+                subTitle: project.tagline,
+                description: project.description,
+                startDate: project.startDate,
+                endDate: project.endDate,
+                imageUrl: project.imageUrl,
+                tags: project.tags
+            };
+        });
+    }
+
     ngOnInit() {
         this.projects = this.projectsService.list();
+        this.projectItems = this.projects.map(projects => this.getDisplayItems(projects));
     }
 }
