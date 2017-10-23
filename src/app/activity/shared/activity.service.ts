d2156b8bf1659a6870de8b14d47fe3ae4bddd162
Kevin Lutzer <kml183@mail.usask.ca>
2017-10-22 22:49:00 -0600

    update activity to through snackbar

--- src/app/activity/shared/activity.service.ts
+++ src/app/activity/shared/activity.service.ts
@@ -3,12 +3,21 @@ import { Observable } from 'rxjs/Observable';
 
 import { Activity } from './activity.model';
 import { ActivityApiService } from './activity.api.service';
+import { AlertService } from './../../core';
 
 @Injectable()
 export class ActivityService {
-    constructor(private activityApiService: ActivityApiService) {}
+    constructor(
+        private activityApiService: ActivityApiService,
+        private alertService: AlertService
+    ) {}
 
     public list(): Observable<Activity[]> {
-        return this.activityApiService.getAllActivities();
+        const activities = this.activityApiService.getAllActivities();
+        activities.subscribe(
+            () => null,
+            (error) => this.alertService.throwErrorSnack('Oops! We were not able to load my experience!')
+        );
+        return activities;
     }
 }
