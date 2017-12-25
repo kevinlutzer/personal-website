851ce7df056519513b5fb7544fbd21de22f7b46f
Kevin Lutzer <kml183@mail.usask.ca>
2017-12-25 17:38:49 -0600

    added activity components

---
+++ src/app/activity/details/activity-details.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+
+import { Activity } from '../shared/activity.model';
+
+export type SeeMoreButtonText = 'See More!' | 'Hide';
+
+@Component({
+  selector: 'activity-details',
+  templateUrl: './activity-details.component.html'
+})
+
+export class ActivityDetailsComponent {
+
+  @Input() activity: Activity;
+
+  contentButtonText: SeeMoreButtonText = 'See More!';
+  showContent = false;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private registry: MatIconRegistry
+  ) { }
+
+  public navigateToActionLink(): void {
+    window.open(this.activity.activityUrl);
+  }
+
+  public onShowContent() {
+    this.contentButtonText = this.contentButtonText === 'See More!' ? 'Hide' : 'See More!';
+    this.showContent = !this.showContent;
+  }
+
+  get dateString(): string {
+    let result = '';
+    if ( this.activity.dateFinish && this.activity.dateStart ) {
+      result = '( ' + this.activity.dateStart + ' - ' + this.activity.dateFinish + '  )';
+    } else if ( this.activity.dateStart ) {
+      result = '( ' + this.activity.dateStart + ' - Present )';
+    }
+    return result;
+  }
+}
