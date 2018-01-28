b6a7287ce705b97ae3da7269166c5f74b4615d5b
kml183 <kml183@mail.usask.ca>
2018-01-27 18:52:55 -0600

    get the nav to actually work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -11,16 +11,10 @@ export class AppComponent {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
   ) {}
 
   navigateToUrl(url: string): void {
     this.router.navigateByUrl(url);
-    this.isActiveTab(url);
   }
 
-  isActiveTab(url: string): boolean {
-    console.log(this.route.snapshot);
-    return false;
-  }
 }
