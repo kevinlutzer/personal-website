550ffc44c45ef6917d5096207f1d5609fe0ea81f
kml183 <kml183@mail.usask.ca>
2018-02-17 00:10:32 -0600

    refactor so this thing builds

--- src/app/project/index.ts
+++ src/app/project/index.ts
@@ -1,7 +1,6 @@
 export {ProjectService} from './shared/project.service';
 export {ProjectStubService} from './shared/project.stub.service';
 export {ProjectApiService} from './shared/project.api.service';
-export {ProjectListComponent} from './project-list/project-list.component';
 export {Project} from './shared/project.model';
 export {ProjectDetailsComponent} from './project-details/project-details.component';
 export {ProjectRoutingModule} from './project-routing.module';
