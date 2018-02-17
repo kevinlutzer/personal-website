2ca7dff3338724a0cd6398d20900c5039b672b4a
kml183 <kml183@mail.usask.ca>
2018-02-16 23:51:17 -0600

    minor refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,21 @@ import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <mat-toolbar 
+      fxLayout="row"
+      fxLayoutAlign="space-between"
+      fxLayoutContainer
+      color="primary">
+      <nav>
+          <button mat-button routerLink="/overview">Overview</button>
+          <button mat-button routerLink="/projects">Projects</button>
+          <button mat-button routerLink="/about">About</button>
+      </nav>
+      <social-media></social-media>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
