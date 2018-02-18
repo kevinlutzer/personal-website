9b6c6c9e4b9a57d90c644907ad9e74774070d563
kml183 <kml183@mail.usask.ca>
2018-02-17 23:07:48 -0600

    refactor some more

---
+++ src/app/visitor/visitor-table.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { Visitor } from './core/visitor.model';
+
+@Component({
+  selector: 'visitor-table',
+  templateUrl: './visitor-table.component.html',
+  styles: []
+})
+export class VisitorTableComponent implements AfterViewInit {
+    private _visitors: Visitor[];
+    public dataSource = new MatTableDataSource();
+    public displayedColumns = [
+        'type', 'location'
+    ]
+
+    @ViewChild('mat-paginator') private paginator: MatPaginator;
+    @Input() set visitors(value: Visitor[]) {
+        this.dataSource.data = value;
+    }
+
+    ngAfterViewInit(): void {
+        this.dataSource.paginator = this.paginator;
+    }
+    
+}
