436604aa634354131f910b2d42bae6a726297592
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-09-16 17:32:14 -0600

    add express app with base handler

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const create_visitor_1 = require("./api/create_visitor");
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
+const app = express();
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+// build multiple CRUD interfaces:
+app.get('/', create_visitor_1.CreateVisitor);
+// Expose Express API as a single Cloud Function:
+exports.Visitor = functions.https.onRequest(app);
+// return db
+//     .collection('Visitor')
+//     .doc(id)
+//     .set(data)
+//     .then(
+//         resp => console.log("Added new entity with id: ", id, " and resp: ", resp),
+//         err => console.error("Failed to create entity: ", err)
+//     );
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
 import * as functions from 'firebase-functions';
+import * as admin   from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+import {CreateVisitor} from './api/create_visitor';
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+db.settings({timestampsInSnapshots: true});
+
+const app = express();
+
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// build multiple CRUD interfaces:
+app.get('/', CreateVisitor);
+
+// Expose Express API as a single Cloud Function:
+exports.Visitor = functions.https.onRequest(app);
+
+
+// return db
+//     .collection('Visitor')
+//     .doc(id)
+//     .set(data)
+//     .then(
+//         resp => console.log("Added new entity with id: ", id, " and resp: ", resp),
+//         err => console.error("Failed to create entity: ", err)
+//     );
