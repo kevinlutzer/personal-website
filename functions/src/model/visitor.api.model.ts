21c71a222463617b4eae924cb02c67a2241b1aca
Kevin Lutzer <kevinlutzer9@gmail.com>
2019-02-03 18:13:48 -0600

    update model file

--- functions/src/model/visitor.api.model.ts
+++ functions/src/model/visitor.api.model.ts
@@ -1,15 +1,17 @@
-
+import { isBoolean } from "util";
 
 export const MODEL = 'Visitor';
 
 export class Visitor {
     ip: string;
     type: string;
+    created: FirebaseFirestore.Timestamp
 }
 
 export function convertFirebaseVisitor(data: FirebaseFirestore.DocumentData) {
     const obj = new Visitor()
     obj.ip = data.ip;
     obj.type = data.type;
+    obj.created = data.created
     return obj;
 }
