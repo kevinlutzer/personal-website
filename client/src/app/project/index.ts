4fb608c10a1b02af3bac104d17cdb84f8deab533
Kevin Lutzer <kevinlutzer9@gmail.com>
2019-02-19 21:29:59 -0600

    get it to build

--- client/src/app/project/index.ts
+++ client/src/app/project/index.ts
@@ -1,5 +1,4 @@
 export {ProjectService} from './shared/project.service';
-export {ProjectApiService} from './shared/project.api.service';
 export {Project} from './shared/project.model';
 export {ProjectDetailsComponent} from './project-details/project-details.component';
 export {ProjectRoutingModule} from './project-routing.module';
