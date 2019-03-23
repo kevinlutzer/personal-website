db40b278caeee96f3916b37d7cae3e4c6f3d5313
Kevin Lutzer <kevinlutzer9@gmail.com>
2019-03-23 13:52:12 -0600

    refactor

--- client/src/app/main-nav/main-nav.component.ts
+++ client/src/app/main-nav/main-nav.component.ts
@@ -12,6 +12,10 @@ import { DomSanitizer } from '@angular/platform-browser';
       height: 100%;
     }
 
+    .hidden {
+      display: none;
+    }
+
     .sidenav {
       width: 200px;
       box-shadow: 3px 0 6px rgba(0,0,0,.24);
