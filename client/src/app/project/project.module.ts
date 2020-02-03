0c6c0d5b141380e99703e60a124bbcad6249854d
Kevin Lutzer <klutzer@vendasta.com>
2020-02-03 11:24:02 -0500

    move things around

--- client/src/app/project/project.module.ts
+++ client/src/app/project/project.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatChipsModule, MatButtonModule, MatCardModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ProjectListComponent } from './project-list.component';
+import { ProjectListComponent } from './list/project-list.component';
 import { ProjectService } from './shared/project.service';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { SharedModule as ProjectSharedModule } from '../shared';
 import { CoreModule as ProjectCoreModule } from '../core';
-import { ProjectSampleComponent } from './project-sample/project-sample.component';
-import { DeviceStatusCard } from './project-sample/device-status-card/device-status-card.component';
-import { StatusService, TelemetryService } from './project-sample/room-environment-monitor';
+import { ProjectSampleComponent } from './sample/project-sample.component';
+import { DeviceStatusCard } from './sample/device-status-card/device-status-card.component';
+import { StatusService, TelemetryService } from './sample/room-environment-monitor';
 import { environment } from '../../environments/environment';
 
 @NgModule({
