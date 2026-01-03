2623437bfc7f2cf8632d985bcf02f877bbffdc64
Kevin Lutzer <kevinlutzer@proton.me>
2026-01-03 18:52:21 -0500

    add shocked labs

--- web/src/app/overview/activity/activity.service.ts
+++ web/src/app/overview/activity/activity.service.ts
@@ -10,6 +10,13 @@ export class ActivityService {
   public get experience$(): Observable<Activity[]> {
     return of([
       {
+        title: 'Senior Firmware Engineer',
+        company: 'Shocked Labs',
+        activityImgUrl: '/assets/icons/shocked_labs_48x48.png',
+        description: 'I help companies build dependable hardware and firmware for industrial and connected products. My consulting work spans PLCs, embedded devices, and IoT systems. I cover everything from hardware architecture, to prototyping, debugging and deployment.',  
+        dateStart: new Date(2025, 12, 1, 1, 1, 1, 1)
+      } as Activity,
+      {
         title: 'Software Developer',
         company: 'Nureva',
         activityImgUrl: '/assets/icons/nureva_48x48.jpg',
